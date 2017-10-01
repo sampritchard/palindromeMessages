a8cff639af21fc944a5f63f41e3fb683f32ca5e2
Sam Pritchard <sampritchard@msn.com>
2017-10-01 05:44:15 +0100

    Split controllers, routes & views. Added a controller so routes just define routes. Added test coverage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
+const palindromeController = require('../controllers/palindromeController')
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('indexPage');
-})
+router.get('/', palindromeController.indexPage)
 
 module.exports = router;
