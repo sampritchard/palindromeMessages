0871504db725e1c85eeea1b071acacc10daa4a3b
Sam Pritchard <sampritchard@msn.com>
2017-10-01 06:56:04 +0100

    Sends user to an error page if an empty string is passed into the input

--- controllers/palindromeController.js
+++ controllers/palindromeController.js
@@ -6,7 +6,9 @@ exports.newPalindrome = (req, res) => {
   const word = req.body.palindrome;
   const removePunctuation = word.replace(/[^A-Za-z]+/g,'').toLowerCase();
   const reverseWord = removePunctuation.split('').reverse().join('')
-  if (removePunctuation === reverseWord) {
+  if (word.length === 0) {
+    res.render('errorPage')
+  } else if(removePunctuation === reverseWord) {
     res.render('truePage')
   } else {
     res.render('falsePage')
--- test/palindromeEdgeCasesSpec.js
+++ test/palindromeEdgeCasesSpec.js
@@ -1,5 +1,6 @@
 const Browser = require('zombie');
 const app = require('../server');
+const throwError = new Error("Enter a string!")
 
 Browser.localhost('localhosr', 3000);
 
@@ -34,5 +35,22 @@ Browser.localhost('localhosr', 3000);
 
     it('can returns the false Page', () => {
       browser.assert.text('h1', 'TRUE!')
+    })
+  })
+
+  describe('User enters a blank form', () => {
+    const browser = new Browser();
+
+    before(() => {
+      return browser.visit('/');
+    })
+
+    before( async() => {
+      browser.fill('palindrome', '');
+      await browser.pressButton('Submit');
+    })
+
+    it('can returns the false Page', () => {
+      browser.assert.text('h1', 'ERROR! Please enter a valid string')
     })
   })
